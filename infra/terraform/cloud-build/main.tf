7392b6162aaec7286bf53222251e7c5cdc9f6334
svz <143542619+vonzweigbergksamuel@users.noreply.github.com>
2025-10-29 14:23:09 +0100

    WIP

--- infra/terraform/cloud-build/main.tf
+++ infra/terraform/cloud-build/main.tf
@@ -1,12 +1,13 @@
-resource "google_secret_manager_secret" "github_token" {
-  secret_id = "github-token"
+resource "google_secret_manager_secret" "github_token_secret" {
+  secret_id = "github-token-secret"
+
   replication {
     auto {}
   }
 }
 
-resource "google_secret_manager_secret_version" "github_token_version" {
-  secret      = google_secret_manager_secret.github_token.id
+resource "google_secret_manager_secret_version" "github_token_secret_version" {
+  secret      = google_secret_manager_secret.github_token_secret.id
   secret_data = file("${path.module}/github-token.txt")
 }
 
@@ -14,27 +15,32 @@ data "google_project" "default" {
   project_id = var.project_id
 }
 
+data "google_iam_policy" "p4sa_secret_accessor" {
+  binding {
+    role = "roles/secretmanager.secretAccessor"
+    members = ["serviceAccount:service-${data.google_project.default.number}@gcp-sa-cloudbuild.iam.gserviceaccount.com"]
+  }
+}
+
+resource "google_secret_manager_secret_iam_policy" "policy" {
+  secret_id   = google_secret_manager_secret.github_token_secret.secret_id
+  policy_data = data.google_iam_policy.p4sa_secret_accessor.policy_data
+}
+
+resource "google_cloudbuildv2_connection" "github" {
+  location = var.region
+  name     = "github-connection"
+
+  github_config {
+    authorizer_credential {
+      oauth_token_secret_version = google_secret_manager_secret_version.github_token_secret_version.id
+    }
+  }
+}
+
 resource "google_cloudbuildv2_repository" "github" {
   location          = var.region
   name              = "github-repo"
-  parent_connection = "projects/${var.project_id}/locations/${var.region}/connections/github"
+  parent_connection = google_cloudbuildv2_connection.github.name
   remote_uri        = "https://github.com/${var.github_repo}.git"
-}
-
-resource "google_cloudbuild_trigger" "docker_build" {
-  location   = var.region
-  project    = var.project_id
-  name       = "docker-build-trigger"
-  filename   = "cloudbuild.yaml"
-
-  trigger_template {
-    branch_name = "main"
-    repo_name   = google_cloudbuildv2_repository.github.name
-  }
-
-  depends_on = [google_cloudbuildv2_repository.github]
-}
-
-output "cloud_build_trigger_id" {
-  value = google_cloudbuild_trigger.docker_build.id
 }
