57eb6762e6d87715cbabfae088c5d3e61641c702
svz <143542619+vonzweigbergksamuel@users.noreply.github.com>
2025-11-04 13:32:34 +0100

    WIP

--- infra/terraform/gke/cluster.tf
+++ infra/terraform/gke/cluster.tf
@@ -55,7 +55,7 @@ resource "google_container_node_pool" "default_pool" {
   node_config {
     machine_type = "e2-small"
     disk_size_gb = 30
-    disk_type    = "pd-balanced"
+    disk_type    = "pd-standard"
 
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform",
