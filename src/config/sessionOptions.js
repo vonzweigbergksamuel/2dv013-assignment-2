59e8a3361150dcfe16bbe2421ca5e91768623ec2
svz <143542619+vonzweigbergksamuel@users.noreply.github.com>
2025-11-13 15:34:32 +0100

    Refactor sessionOptions: update cookie sameSite attribute to 'lax' and clean up commented code

--- src/config/sessionOptions.js
+++ src/config/sessionOptions.js
@@ -9,16 +9,12 @@ await redisClient.connect();
 // Options object for the session middleware.
 export const sessionOptions = {
 	store: new RedisStore({ client: redisClient }),
-	name: process.env.SESSION_NAME, // Don't use default session cookie name.
-	secret: process.env.SESSION_SECRET, // Change it!!! The secret is used to hash the session with HMAC.
-	resave: false, // Resave even if a request is not changing the session.
-	saveUninitialized: false, // Don't save a created but not modified session.
+	name: process.env.SESSION_NAME,
+	secret: process.env.SESSION_SECRET,
+	resave: false,
+	saveUninitialized: false,
 	cookie: {
 		maxAge: 1000 * 60 * 60 * 24, // 1 day
-		sameSite: "strict",
+		sameSite: "lax",
 	},
 };
-
-// if (process.env.NODE_ENV === "production") {
-// 	sessionOptions.cookie.secure = true; // serve secure cookies
-// }
